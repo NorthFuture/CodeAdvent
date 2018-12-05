69afb745da58d52434b7e3eff81f5c1ed78a8008
prevedelloc <cristian.prevedello@previnet.it>
2018-12-05 13:11:32 +0100

    refactor Day 4

--- src/main/scala/Year2018/Day4.scala
+++ src/main/scala/Year2018/Day4.scala
@@ -21,51 +21,66 @@ object Day4 extends App {
 
   case class WakesUp(date: OffsetDateTime) extends State
 
+  case class Guard(id: Int, totalSleepTime: Int = 0, sleepTimeByMinutes: Map[Int, Int] = Map()) {
+    def sleep(minute: Int): Guard = {
+      this.copy(sleepTimeByMinutes = sleepTimeByMinutes + ((minute, sleepTimeByMinutes.getOrElse(minute, 0) + 1)))
+    }
+
+    def sleep(fromMinute: Int, toMinute: Int): Guard = {
+      (fromMinute to toMinute).foldLeft(this)((s, m) => s.sleep(m)).copy(totalSleepTime = totalSleepTime + toMinute - fromMinute)
+    }
+  }
+
   val input = Source.fromFile("data/Year2018/Day4.txt").getLines().map { x =>
     x match {
       case regBeingShift(y, m, d, h, mm, id) => BeginShift(id.toInt, OffsetDateTime.of(y.toInt, m.toInt, d.toInt, h.toInt, mm.toInt, 0, 0, ZoneOffset.UTC))
       case regFallsAsleep(y, m, d, h, mm) => FallsAsleep(OffsetDateTime.of(y.toInt, m.toInt, d.toInt, h.toInt, mm.toInt, 0, 0, ZoneOffset.UTC))
       case regWakesUp(y, m, d, h, mm) => WakesUp(OffsetDateTime.of(y.toInt, m.toInt, d.toInt, h.toInt, mm.toInt, 0, 0, ZoneOffset.UTC))
-
     }
   }.toList
     .sortBy(_.date)
 
+  def getOrCreateGuard(guardId: Int)(implicit guards: Set[Guard]): Guard = {
+
+    guards.find(_.id == guardId) match {
+      case None => Guard(guardId)
+      case Some(x) => x
+    }
+  }
+
   @tailrec
-  def solveStrategy1(guardOnDuty: Int, lastTime: OffsetDateTime, input: List[State], sleepMinutes: Map[Int, Int], minutesAsleep: Map[(Int, Int), Int]): (Map[Int, Int], Map[(Int, Int), Int]) = {
+  def solveStrategy1(guardOnDuty: Guard, lastTime: OffsetDateTime, input: List[State])(implicit guards: Set[Guard]): Set[Guard] = {
     input match {
       case Nil =>
-        (sleepMinutes, minutesAsleep)
+        guards
       case h :: tail => h match {
         case x: BeginShift =>
-          solveStrategy1(x.id, x.date, tail, sleepMinutes, minutesAsleep)
+          solveStrategy1(getOrCreateGuard(x.id), x.date, tail)(guards)
         case x: FallsAsleep =>
-          solveStrategy1(guardOnDuty, x.date, tail, sleepMinutes, minutesAsleep)
+          solveStrategy1(guardOnDuty, x.date, tail)(guards)
         case x: WakesUp =>
           val sleepTime: Int = ((x.date.toEpochSecond - lastTime.toEpochSecond) / 60).toInt
 
-          val existingSleepTime = sleepMinutes.getOrElse(guardOnDuty, 0) + sleepTime
+          val newGuardOnDuty = guardOnDuty.sleep(lastTime.getMinute, lastTime.getMinute + sleepTime - 1)
 
-          val newMinutesAsleep = (lastTime.getMinute to lastTime.getMinute + sleepTime - 1).foldLeft(minutesAsleep)((s, x) => s + (((guardOnDuty, x), s.getOrElse((guardOnDuty, x), 0) + 1)))
-
-          solveStrategy1(guardOnDuty, x.date, tail, sleepMinutes + ((guardOnDuty, existingSleepTime)), newMinutesAsleep)
+          solveStrategy1(newGuardOnDuty, x.date, tail)(guards + (newGuardOnDuty))
       }
     }
   }
 
-  val state = solveStrategy1(input.head.asInstanceOf[BeginShift].id, input.head.asInstanceOf[BeginShift].date, input.tail, Map[Int, Int](), Map[(Int, Int), Int]())
+  implicit val initialState = Set[Guard]()
 
-  val g1 = state._1.toList.maxBy(_._2)
+  val finalState = solveStrategy1(getOrCreateGuard(input.head.asInstanceOf[BeginShift].id), input.head.asInstanceOf[BeginShift].date, input.tail)
 
-  val m1 = state._2.filter(_._1._1 == g1._1).maxBy(_._2)._1._2
+  val g1 = finalState.maxBy(_.totalSleepTime)
 
   println(g1)
-  println(g1._1 * m1)
+  println(g1.id * g1.sleepTimeByMinutes.maxBy(_._2)._2)
 
-  val g2 = state._2.toList.maxBy(_._2)
+  val g2 = finalState.maxBy(x => x.sleepTimeByMinutes.maxBy(_._2))
 
   println(g2)
-  println(g2._1._1 * g2._1._2)
+  println(g2.id * g2.sleepTimeByMinutes.maxBy(_._2)._2)
 
 }
 
